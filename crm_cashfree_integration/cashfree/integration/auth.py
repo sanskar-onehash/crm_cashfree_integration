eba49cd9f2ed78cef196b60700404822c4bcb09e
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-26 18:15:20 +0530

    feat(webhook): add webhook verification decorator
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_cashfree_integration/cashfree/integration/auth.py
+++ crm_cashfree_integration/cashfree/integration/auth.py
@@ -1,6 +1,8 @@
 import frappe
-
-from crm_cashfree_integration.cashfree.integration.config import API_VERSION
+import base64
+import hashlib
+import hmac
+from crm_cashfree_integration.cashfree.integration.config import INTEGRATION_VERSION
 
 
 def get_headers(request_id=None, idempotency_key=None):
@@ -12,6 +14,39 @@ def get_headers(request_id=None, idempotency_key=None):
     return {
         "x-client-id": cashfree_integration.get("app_id"),
         "x-client-secret": cashfree_integration.get_password("secret_key"),
-        "x-api-version": API_VERSION,
+        "x-api-version": INTEGRATION_VERSION,
         "Content-Type": "application/json",
     }
+
+
+def verify_webhook(fn):
+    def wrapper(*args, **kwargs):
+        webhook_version = frappe.request.headers.get("x-webhook-version")
+        if not webhook_version or webhook_version != INTEGRATION_VERSION:
+            frappe.log_error("invalid_webhook_version", webhook_version)
+            frappe.throw("Invalid x-webhook-version received.")
+
+        cashfree_integration = frappe.get_single("Cashfree Integration")
+        verifyCashfreesSignature(cashfree_integration.get_password("secret_key"))
+
+        kwargs.pop("cmd")
+        fn(*args, **kwargs)
+
+    return wrapper
+
+
+def verifyCashfreesSignature(secret_key):
+    raw_body = frappe.request.data
+    timestamp = frappe.request.headers["x-webhook-timestamp"]
+    signature = frappe.request.headers["x-webhook-signature"]
+    signature_data = timestamp + raw_body
+    message = bytes(signature_data, "utf-8")
+    secretkey = bytes(secret_key, "utf-8")
+    generatedSignature = base64.b64encode(
+        hmac.new(secretkey, message, digestmod=hashlib.sha256).digest()
+    )
+    computed_signature = str(generatedSignature, encoding="utf8")
+    if computed_signature == signature:
+        json_response = frappe.json.loads(raw_body)
+        return json_response
+    frappe.throw("Generated signature and received signature did not match.")
--- crm_cashfree_integration/cashfree/integration/config.py
+++ crm_cashfree_integration/cashfree/integration/config.py
@@ -1,2 +1,2 @@
-API_VERSION = "2025-01-01"
+INTEGRATION_VERSION = "2025-01-01"
 CLIENT_SCRIPT_SRC = "https://sdk.cashfree.com/js/v3/cashfree.js"
