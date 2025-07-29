b08085e984038bc25922a39f360e1ff87c70ae5a
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-29 11:01:24 +0530

    fix(order): parse-format customer_details payload
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_cashfree_integration/cashfree/integration/api.py
+++ crm_cashfree_integration/cashfree/integration/api.py
@@ -7,7 +7,7 @@ def create_cf_order(
     order_amount: float,
     order_id: str,
     customer_details: dict,
-    order_expiry_time: datetime | None = None,
+    order_expiry_time: str | datetime | None = None,
     order_meta: dict | None = None,
 ):
     if order_expiry_time:
@@ -19,7 +19,7 @@ def create_cf_order(
             "order_amount": order_amount,
             "order_currency": order_currency,
             "order_id": order_id,
-            "customer_details": customer_details,
+            "customer_details": utils.prepare_customer_details(customer_details),
             "order_expiry_time": order_expiry_time,
             "order_meta": order_meta,
         },
--- crm_cashfree_integration/cashfree/integration/utils.py
+++ crm_cashfree_integration/cashfree/integration/utils.py
@@ -16,7 +16,23 @@ def generate_unique_id():
     return uuid.uuid4()
 
 
-def datetime_to_iso(dt: datetime):
+def datetime_to_iso(dt: datetime | str):
+    if isinstance(dt, str):
+        frappe.utils.get_datetime(dt)
     tz = pytz.timezone(frappe.db.get_system_setting("time_zone") or "Asia/Kolkata")
     localized_dt = tz.localize(dt) if dt.tzinfo is None else dt.astimezone(tz)
     return localized_dt.isoformat()
+
+
+def prepare_customer_details(customer_details: dict):
+    customer_id = (customer_details["customer_id"] or "").strip()
+    customer_phone = (customer_details["customer_phone"] or "").strip()
+
+    if not customer_id or not customer_phone:
+        frappe.throw("customer_id and customer_phone are mandatory")
+
+    if " " in customer_id:
+        customer_id = customer_id.replace(" ", "-")
+
+    customer_details["customer_id"] = customer_id
+    return customer_details
