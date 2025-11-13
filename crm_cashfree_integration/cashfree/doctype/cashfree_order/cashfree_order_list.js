582784900c55343c9ee9c6912e017ccb89db5b44
Sanskar Soni <sanskarsoni300@gmail.com>
2025-11-13 08:57:30 +0530

    fix(order): correct status field name
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_cashfree_integration/cashfree/doctype/cashfree_order/cashfree_order_list.js
+++ crm_cashfree_integration/cashfree/doctype/cashfree_order/cashfree_order_list.js
@@ -1,11 +1,11 @@
 frappe.listview_settings["Cashfree Order"] = {
   get_indicator: function (doc) {
-    if (doc.payment_status === "Success") {
-      return [doc.payment_status, "green", "status,=,Success"];
-    } else if (doc.payment_status === "Failed") {
-      return [doc.payment_status, "red", "status,=,Failed"];
-    } else if (doc.payment_status === "User Droped") {
-      return [doc.payment_status, "yellow", "status,=,Expired"];
+    if (doc.status === "Success") {
+      return [doc.status, "green", "status,=,Success"];
+    } else if (doc.status === "Failed") {
+      return [doc.status, "red", "status,=,Failed"];
+    } else if (doc.status === "User Droped") {
+      return [doc.status, "yellow", "status,=,Expired"];
     }
   },
 };
